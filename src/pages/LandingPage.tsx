d1fc4b5dea193883124d8f9aefc90e88d228e944
Stellarography <56196705+Stellarography@users.noreply.github.com>
2025-03-02 08:38:46 -0600

    Initial commit

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import TextField from '@mui/material/TextField';
 import Container from '@mui/material/Container';
+import { useDispatch, useSelector } from 'react-redux';
 import Typography from '@mui/material/Typography';
+import { setInterests } from '../features/user/userSlice';
+import { RootState } from '../app/store';
 
 const LandingPage: React.FC = () => {
+  const dispatch = useDispatch();
+  const interests = useSelector((state: RootState) => state.user.interests);
+
+  const handleInterestsChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch(setInterests(event.target.value));
+  };
 
   return (
     <Container maxWidth="sm">
@@ -13,9 +22,12 @@ const LandingPage: React.FC = () => {
       <TextField
         label="Enter your interests"
         variant="outlined"
+        value={interests}
         fullWidth
         margin="normal"
+        onChange={handleInterestsChange}
       />
+      <Typography variant="body1" margin="normal">Interests: {interests}</Typography>
     </Container>
   );
 };
